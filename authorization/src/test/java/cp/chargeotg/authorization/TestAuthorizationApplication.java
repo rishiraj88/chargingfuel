8b69e2551619df393e7824d3a8a9536ed305850a
Rishi Raj <rishiraj88+github@gmail.com>
2025-03-20 16:02:54 +0100

    Create project skeletons.

---
+++ authorization/src/test/java/cp/chargeotg/authorization/TestAuthorizationApplication.java
@@ -0,0 +1,11 @@
+package cp.chargeotg.authorization;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(AuthorizationApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
