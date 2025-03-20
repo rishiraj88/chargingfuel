1fcc70a473335914de2bb9a34b20095a1f7407d2
Rishi Raj <rishiraj88+github@gmail.com>
2025-03-20 23:32:06 +0100

    Add Kafka properties to Gateway.

--- gateway/src/main/java/cp/chargeotg/gateway/service/GatewayServiceImpl.java
+++ gateway/src/main/java/cp/chargeotg/gateway/service/GatewayServiceImpl.java
@@ -22,13 +22,12 @@ public class GatewayServiceImpl implements GatewayService {
         station id, driver id, callback url
         */
         log.info("Requesting authorization for driver {} at station {}...", chargingSessionReq.driverId(), chargingSessionReq.stationUuid());
+
         //use event for invocation of auth service
         var authorizationCheckEvent = new AuthorizationCheckEvent(chargingSessionReq.stationUuid().toString(), chargingSessionReq.driverId(), chargingSessionReq.callbackUrl().toString());
-
         kafkaTemplate.send(Constants.AUTHORIZATION_CHECK_QUEUE_NAME, authorizationCheckEvent);
-
         log.info("Requested authorization for driver {} at station {}...", chargingSessionReq.driverId(), chargingSessionReq.stationUuid());
 
-        return new ChargingSessionResp("accepted", "Request is being processed asynchronously. The result will be sent to the provided callback URL.");         // authservice sends the decision to callbackurl
+        return new ChargingSessionResp("accepted", "Request is being processed asynchronously. The result will be sent to the provided callback URL.");         // Auth service sends the decision to callback url.
     }
 }
